b4a91f33f7fcb1a811a064c50d7d0d6033c7ab52
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2024-11-09 21:14:07 +0200

    whyy

--- frontend/src/app/components/ui/submit-response.tsx
+++ frontend/src/app/components/ui/submit-response.tsx
@@ -46,6 +46,7 @@ const TicketResponsePage = () => {
     if (pathname.includes("/profile")) { 
       setIsClient(true);
       console.log("CONDITION TRUE"); // Check if we're on the ticket page
+      return;
     } else {
       console.log("CONDITION FALSE");
       setIsClient(true);
