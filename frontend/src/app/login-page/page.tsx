89dd4e7b7d4eda0a4aeb547a53bc06c5bac18720
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2024-11-09 19:29:46 +0200

    Update page.tsx

--- frontend/src/app/login-page/page.tsx
+++ frontend/src/app/login-page/page.tsx
@@ -5,7 +5,7 @@ import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
 
-
+//wwwww
 export default function Login() {
   const radius = 100;
   const [visibleEmail, setVisibleEmail] = useState(false);
