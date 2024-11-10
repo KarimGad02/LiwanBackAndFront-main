e0a0cf2b7f96c9faf3eaae103cb4b64dfd0e5359
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2024-11-10 17:43:48 +0200

    samir is -300iq

--- frontend/src/app/admin-dashboard/page.tsx
+++ frontend/src/app/admin-dashboard/page.tsx
@@ -418,7 +418,7 @@ export function AdminDashboard() {
     <div className="flex h-full">
       {/* Main content */}
       <main
-        className={`flex-1 p-4 transition-all duration-300 dark:bg-Primary dark:text-neutral-200 bg-neutral-200 text-Primary ${isExpanded ? "ml-[300px]" : "ml-[72px] h-full"
+        className={`flex-1 p-4 transition-all duration-300 dark:bg-Primary dark:text-neutral-200 bg-neutral-200 text-Primary ${isExpanded ? "ml-[300px]" : "ml-[72px] overflow-y-auto"
           }`}
       >
         <h1 className="text-3xl md:text-4xl font-bold mb-4 px-8 py-4">
